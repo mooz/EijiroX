b197a06e060ad1533676b7054d3f759f63a2d654
mooz <stillpedant@gmail.com>
2011-01-28 02:02:51 +0900

    now, keyhandler is able to handle the default shortcut keys

--- src/public_html/keyhandler.js
+++ src/public_html/keyhandler.js
@@ -10,109 +10,164 @@ var KeyHandler = {
         this.keyMap[name] = action;
     },
 
-    // ============================================================ //
-
-    isControlKey: function (ev) {
-        return ev.ctrlKey || ev.commandKey;
+    isCharacterKey: function (ev) {
+        return ev.keyCode >= 48 && ev.keyCode <= 90;
     },
 
-    isMetaKey: function (ev) {
-        return ev.altKey || ev.metaKey;
-    },
-
-    isDisplayableKey: function (ev) {
-        return ev.charCode >= 0x20 && ev.charCode <= 0x7e;
-    },
-
-    // ============================================================ //
-
-    specialKeyEventToString: function (ev) {
-        var keyStr;
-
-        // special charactors
-        switch (ev.keyCode) {
-        case ev.DOM_VK_ESCAPE:
-            keyStr = "ESC";
-            break;
-        case ev.DOM_VK_RETURN:
-        case ev.DOM_VK_ENTER:
-            keyStr = "RET";
-            break;
-        case ev.DOM_VK_RIGHT:
-            keyStr = "<right>";
-            break;
-        case ev.DOM_VK_LEFT:
-            keyStr = "<left>";
-            break;
-        case ev.DOM_VK_UP:
-            keyStr = "<up>";
-            break;
-        case ev.DOM_VK_DOWN:
-            keyStr = "<down>";
-            break;
-        case ev.DOM_VK_PAGE_UP:
-            keyStr = "<prior>";
-            break;
-        case ev.DOM_VK_PAGE_DOWN:
-            keyStr = "<next>";
-            break;
-        case ev.DOM_VK_END:
-            keyStr = "<end>";
-            break;
-        case ev.DOM_VK_HOME:
-            keyStr = "<home>";
-            break;
-        case ev.DOM_VK_TAB:
-            keyStr = "<tab>";
-            break;
-        case ev.DOM_VK_BACK_SPACE:
-            keyStr = "<backspace>";
-            break;
-        case ev.DOM_VK_PRINTSCREEN:
-            keyStr = "<print>";
-            break;
-        case ev.DOM_VK_INSERT:
-            keyStr = "<insert>";
-            break;
-        case ev.DOM_VK_PAUSE:
-            keyStr = "<pause>";
-            break;
-        case ev.DOM_VK_DELETE:
-            keyStr = "<delete>";
-        default:
-            break;
-        }
-
-        return keyStr;
+    // https://github.com/decklin/yakshave/blob/master/yakshave.js
+    keyCodeMap: {
+        3: '<Cancel>',
+        6: '<Help>',
+        8: 'DEL',
+        9: 'TAB',
+        12: '<Clear>',
+        13: 'LFD',
+        14: 'RET',
+        16: '<Shift>',
+        17: '<Control>',
+        18: '<Alt>',
+        19: '<pause>',
+        20: '<CapsLock>',
+        27: 'ESC',
+        32: 'SPC',
+        33: '<next>',
+        34: '<prior>',
+        35: '<end>',
+        36: '<home>',
+        37: '<left>',
+        38: '<up>',
+        39: '<right>',
+        40: '<down>',
+        44: '<print>',
+        45: '<insert>',
+        46: '<delete>',
+        48: '0',
+        49: '1',
+        50: '2',
+        51: '3',
+        52: '4',
+        53: '5',
+        54: '6',
+        55: '7',
+        56: '8',
+        57: '9',
+        65: 'a',
+        66: 'b',
+        67: 'c',
+        68: 'd',
+        69: 'e',
+        70: 'f',
+        71: 'g',
+        72: 'h',
+        73: 'i',
+        74: 'j',
+        75: 'k',
+        76: 'l',
+        77: 'm',
+        78: 'n',
+        79: 'o',
+        80: 'p',
+        81: 'q',
+        82: 'r',
+        83: 's',
+        84: 't',
+        85: 'u',
+        86: 'v',
+        87: 'w',
+        88: 'x',
+        89: 'y',
+        90: 'z',
+        93: '<menu>',
+        96: '<kp-0>',
+        97: '<kp-1>',
+        98: '<kp-2>',
+        99: '<kp-3>',
+        100: '<kp-4>',
+        101: '<kp-5>',
+        102: '<kp-6>',
+        103: '<kp-7>',
+        104: '<kp-8>',
+        105: '<kp-9>',
+        106: '<kp-multiply>',
+        107: '<kp-add>',
+        108: '<kp-backtab>',
+        109: '<kp-subtract>',
+        110: '<kp-decimal>',
+        111: '<kp-divide>',
+        112: '<f1>',
+        113: '<f2>',
+        114: '<f3>',
+        115: '<f4>',
+        116: '<f5>',
+        117: '<f6>',
+        118: '<f7>',
+        119: '<f8>',
+        120: '<f9>',
+        121: '<f10>',
+        122: '<f11>',
+        123: '<f12>',
+        124: '<f13>',
+        125: '<f14>',
+        126: '<f15>',
+        127: '<f16>',
+        128: '<f17>',
+        129: '<f18>',
+        130: '<f19>',
+        131: '<f20>',
+        132: '<f21>',
+        133: '<f22>',
+        134: '<f23>',
+        135: '<f24>',
+        144: '<NumLock>',
+        145: '<Scroll_Lock>',
+        186: ';',
+        187: '=',
+        188: ',',
+        190: '.',
+        191: '/',
+        192: '`',
+        219: '[',
+        220: '\\',
+        221: ']',
+        222: '\'',
+        224: '<Meta>'
     },
 
     keyEventToString: function (ev) {
         var keyStr;
 
-        if (this.isDisplayableKey(ev)) {
-            keyStr = (ev.charCode === 0x20) ? "SPC" : String.fromCharCode(ev.charCode);
-        } else if (ev.keyCode >= ev.DOM_VK_F1 && ev.keyCode <= ev.DOM_VK_F24) {
-            keyStr = "<f" + (ev.keyCode - ev.DOM_VK_F1 + 1) + ">";
-        } else {
-            keyStr = this.specialKeyEventToString(ev);
-        }
+        keyStr = this.keyCodeMap[ev.keyCode];
 
         if (!keyStr)
             return null;
 
-        // append modifier
-        if (this.isMetaKey(ev))
-            keyStr = "M-" + keyStr;
-        if (this.isControlKey(ev))
-            keyStr = "C-" + keyStr;
-        if (ev.shiftKey && (!this.isDisplayableKey(ev) || ev.charCode == 0x20))
-            keyStr = "S-" + keyStr;
+        keyStr = (this.getModifier() || "") + keyStr;
 
         return keyStr;
     },
 
+    getModifier: function () {
+        var modifier = "";
+
+        for (var mod in this.modifierStatus) {
+            if (this.modifierStatus.hasOwnProperty(mod) && this.modifierStatus[mod]) {
+                modifier = mod + "-" + modifier;
+            }
+        }
+
+        return modifier;
+    },
+
+    editableTypes: {
+        password : true,
+        text     : true
+    },
+
     inEditMode: function () {
-        return (document.activeElement.localName === "input");
+        var elem = document.activeElement;
+
+        return elem.localName === "input" &&
+            (!elem.hasAttribute("type") || this.editableTypes[elem.getAttribute("type")]);
     },
 
     killEvent: function (ev) {
@@ -120,27 +175,64 @@ var KeyHandler = {
         ev.preventDefault();
     },
 
-    handleEvent: function (ev) {
-        console.log("Handled " + ev);
+    modifierStatus: {
+        C: false,                 // control
+        M: false,                 // alt
+        S: false                  // shift
+    },
 
-        var keyStr = this.keyEventToString(ev);
+    modifierMap: {
+        16  : "S",              // shit
+        17  : "C",              // control
+        18  : "M",              // alt
+        224 : "M"               // meta
+    },
 
-        console.log("Name is " + keyStr);
+    handleKeyUp: function (ev) {
+        if (this.modifierMap.hasOwnProperty(ev.keyCode)) {
+            this.modifierStatus[this.modifierMap[ev.keyCode]] = false;
+            console.log("Modifier released " + this.modifierMap[ev.keyCode]);
+        }
+    },
 
-        if (this.keyMap.hasOwnProperty(keyStr)) {
-            var action = this.actions[this.keyMap[keyStr]];
+    handleKeyDown: function (ev) {
+        if (this.modifierMap.hasOwnProperty(ev.keyCode)) {
+            // handle modifier
+            console.log("Modifier pressed " + this.modifierMap[ev.keyCode]);
+            this.modifierStatus[this.modifierMap[ev.keyCode]] = true;
+        } else {
+            // normal
+            var keyStr = this.keyEventToString(ev);
 
-            if (action) {
-                if (this.isDisplayableKey(ev) &&  this.inEditMode()) {
-                    return console.log("Does not allowed");
+            console.log("Key Pressed " + keyStr);
+
+            if (this.keyMap.hasOwnProperty(keyStr)) {
+                var action = this.actions[this.keyMap[keyStr]];
+
+                if (action) {
+                    if ((this.isCharacterKey(ev) && !this.getModifier()) && this.inEditMode()) {
+                        return;
+                    }
+
+                    this.killEvent(ev);
+                    action();
                 }
-
-                console.log("Call action for " + keyStr);
-                this.killEvent(ev);
-                action();
             }
+        }
+    },
+
+    handleEvent: function (ev) {
+        switch (ev.type) {
+        case "keydown":
+            this.handleKeyDown(ev);
+            break;
+        case "keyup":
+            this.handleKeyUp(ev);
+            break;
         }
     }
 };
 
-document.addEventListener('keypress', KeyHandler, true);
+document.addEventListener('keydown', KeyHandler, true);
+document.addEventListener('keyup', KeyHandler, true);
+
