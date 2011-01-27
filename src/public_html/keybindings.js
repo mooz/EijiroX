73f777eccfff9376d8b5b10b83b115d6ea6ead8b
mooz <stillpedant@gmail.com>
2011-01-28 02:02:59 +0900

    some key bindings

--- src/public_html/keybindings.js
+++ src/public_html/keybindings.js
@@ -2,12 +2,9 @@ KeyHandler.addKeyBind("C-n", "scroll_down");
 KeyHandler.addKeyBind("C-p", "scroll_up");
 
 KeyHandler.addKeyBind("C-g", "blur");
-KeyHandler.addKeyBind("C-c", "blur");
 
 KeyHandler.addKeyBind("j", "scroll_down");
 KeyHandler.addKeyBind("k", "scroll_up");
 
-KeyHandler.addKeyBind("M-j", "scroll_down");
-KeyHandler.addKeyBind("M-k", "scroll_up");
-
+KeyHandler.addKeyBind("C-c", "reset");
 KeyHandler.addKeyBind("f", "reset");
